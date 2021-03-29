041c0bb383a9bb3fbb052b0ff51ddf16ec8b183d
M <michael.willy@gmail.com>
2021-03-29 16:21:19 -0400

    dict fn for param storage

--- nameSurvey/nameSurvey/nameFind.py
+++ nameSurvey/nameSurvey/nameFind.py
@@ -4,21 +4,24 @@ import numpy as np
 from nameView import nameView
 
 
+def params():
+    d = {
+         'olden':((0,6),1,(1890,1945),5,'M'),
+         'midcentury':((0,6),1,(1946,1999),5,'M'),
+         # 'comprehensive':((0,6),1,(0,0),3,'M'),
+         }
+    return d
+
+
+
 if __name__ == '__main__':
     bb = nameView()
-    print('test1:')
-    test1 = bb.load_names((0,6),1,(1890,1945),3,'M')
-    print('test2:')
-    test2 = bb.load_names((0,6),1,(1946,1999),3,'M')
-    print('test3:')
-    test3 = bb.load_names((0,6),1,(0,0),3,'M')
+    d = params()
+    # print('test1:')
+    # test1 = bb.load_names(*d['olden'])
+    # a = np.unique(test1['name'])
 
-
-    a = np.unique(test1['name'])
-    b = np.unique(test2['name'])
-    c = np.unique(test3['name'])
-
-    print('\nunique names:',
-            a.shape[0],
-            b.shape[0],
-            c.shape[0])
+    print('\nunique names per set: ')
+    [print('unique names in ',key,': ',
+            np.unique(bb.load_names(*vals)['name']).shape[0])
+            for key,vals in d.items() ]
